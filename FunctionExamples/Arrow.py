8c104febf4edf8c2a919372ae32547bdd9368c94
liyinchen <liyc@liyinchen.com>
2018-11-28 18:02:10 +0800

    增加xlabel,ylabel

--- FunctionExamples/Arrow.py
+++ FunctionExamples/Arrow.py
@@ -9,23 +9,23 @@ fig, ax = plt.subplots()
 # fc: filling color
 # ec: edge color
 bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)
-ax.text(-2, -2, "Sample A", ha="center", va="center", size=20,
+ax.text(2, 2, "Sample A", ha="center", va="center", size=20,
     bbox=bbox_props)
-ax.text(2, 2, "Sample B", ha="center", va="center", size=20,
+ax.text(6, 6, "Sample B", ha="center", va="center", size=20,
     bbox=bbox_props)
  
 # lw: line width
 bbox_props = dict(boxstyle="rarrow", fc=(0.8, 0.9, 0.9), ec="r", lw=2)
-t = ax.text(0, 0, "Direction", ha="center", va="center", rotation=45,
+t = ax.text(4, 4, "Direction", ha="center", va="center", rotation=45,
       size=15,
       bbox=bbox_props)
  
 bb = t.get_bbox_patch()
 bb.set_boxstyle("rarrow", pad=0.6)
 
-A = np.array([1,-3])
-B = np.array([3,-1])
-C = np.array([2,0])
+A = np.array([5,1])
+B = np.array([7,3])
+C = np.array([6,4])
 
 #s : 箭尾的注释 
 #xy : 箭头坐标 
@@ -40,7 +40,9 @@ ax.grid()
 ax.set_aspect('equal')
 
 #default show range（0，1）
-ax.set_xlim(-4, 4)
-ax.set_ylim(-4, 4)
+plt.xlabel('X')
+plt.ylabel('Y')
+ax.set_xlim(0, 8)
+ax.set_ylim(0, 8)
 
 plt.show()
