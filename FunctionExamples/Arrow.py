4361b887158f11d5c52f2c098e2892a86d993264
liyinchen <liyc@liyinchen.com>
2018-11-28 17:38:34 +0800

    画图注释

--- FunctionExamples/Arrow.py
+++ FunctionExamples/Arrow.py
@@ -1,6 +1,10 @@
+#注释有三种，分别为:
+#ax.text()：文本注释，只能填写文本
+#ax.arrow()：箭头标记，不能填写文本 
+#ax.annotate()：箭头注释，在箭头的位置可以填写文本
 import numpy as np
 import matplotlib.pyplot as plt
- 
+
 fig, ax = plt.subplots()
 # fc: filling color
 # ec: edge color
@@ -18,6 +22,22 @@ t = ax.text(0, 0, "Direction", ha="center", va="center", rotation=45,
  
 bb = t.get_bbox_patch()
 bb.set_boxstyle("rarrow", pad=0.6)
+
+A = np.array([1,-3])
+B = np.array([3,-1])
+C = np.array([2,0])
+
+#s : 箭尾的注释 
+#xy : 箭头坐标 
+#xytext : 箭尾坐标 
+#arrowprops：设置箭头标致的格式（字典）
+ax.annotate("", xy=(B[0], B[1]), xytext=(A[0], A[1]),arrowprops=dict(arrowstyle="->"))
+
+ax.arrow(A[0], A[1], C[0]-A[0], C[1]-A[1],
+             length_includes_head=True,# 增加的长度包含箭头部分
+             head_width=0.25, head_length=0.5, fc='r', ec='b')
+ax.grid()
+ax.set_aspect('equal')
 
 #default show range（0，1）
 ax.set_xlim(-4, 4)
