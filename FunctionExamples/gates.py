dbea8b79c49ff8a243127825ddd52874116cc597
liyinchen <liyc@liyinchen.com>
2018-11-29 20:34:52 +0800

    实现与门、非门、与非门和异或门等基础的感知器

---
+++ FunctionExamples/gates.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+if __name__ == '__main__':
+    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = NAND(xs[0], xs[1])
+        
+
+if __name__ == '__main__':
+    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = OR(xs[0], xs[1])
+        print("AND:" + str(xs) + " -> " + str(AND(xs[0], xs[1])))
+        print("OR:" + str(xs) + " -> " + str(OR(xs[0], xs[1])))
+        print("NAND:" + str(xs) + " -> " + str(NAND(xs[0], xs[1])))
+        print("XOR = AND(NAND + OR): " + str(xs) + " -> " + str(XOR(xs[0], xs[1])))
+        print()
