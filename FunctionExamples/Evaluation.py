50cc98686c6f6c39792f4be558033b275ab50e08
liyinchen <liyc@liyinchen.com>
2019-01-03 17:40:07 +0800

    改成函数调用的形式

--- FunctionExamples/Evaluation.py
+++ FunctionExamples/Evaluation.py
@@ -8,52 +8,58 @@ def delElems(data, index):
         del(trdata[i])
     return trdata
 
-data=[random.randint(0,100) for _ in range(100) ]
-print("initial data")
-print(data)
-
-
 #留出法
-#随机采样
-print("留出法")
-index = [random.randint(0,99) for _ in range(36) ]
-tsdata = []
-for i in index:
-    tsdata.append(data[i])
-print("sample tsdata")
-print(tsdata)
-print("sample index")
-print(index)
+def holdOut(data):
+    print("留出法")
+    #随机采样
+    index = [random.randint(0,99) for _ in range(36) ]
+    tsdata = []
+    for i in index:
+        tsdata.append(data[i])
 
-#剔除采样数据
-trdata = delElems(data, index)
-print("training trdata")
-print(trdata)
-
+    #剔除采样数据
+    trdata = delElems(data, index)
+    return tsdata, trdata
 
 #交叉验证法
-print("交叉验证法")
-for i in range(10):
-    jtsdata = data[i * 10: (i + 1) *10]
-    jtrdata = data[0:i * 10] + data[(i + 1) *10:]
-    print("sample jtsdata")
-    print(jtsdata)
-    print("training jtrdata")
-    print(jtrdata)
+def crossValidation(data):
+    print("交叉验证法")
+    jtsdata = []
+    jtrdata = []
     
-    
+    for i in range(10):
+        tsdata = data[i * 10: (i + 1) *10]
+        trdata = data[0:i * 10] + data[(i + 1) *10:]
+        jtsdata.append(tsdata)
+        jtrdata.append(trdata)
+    return jtsdata, jtrdata
+
 #自助法
-print("自助法")
-ztrdata = []
-ztrindex = []
-for i in range(100):
-    a = random.randint(0,99)
-    ztrdata.append(data[a])
-    if a not in ztrindex:
-        ztrindex.append(a)
+def bootStrapping(data):
+    print("自助法")
+    trdata = []
+    index = []
+    for i in range(100):
+        a = random.randint(0,99)
+        trdata.append(data[a])
+        if a not in index:
+            index.append(a)
+        
+    tsdata = delElems(data, index)
+    return tsdata, trdata
+
+def divideData(data, divideFunc):
+    return divideFunc(data)
+
+def main():
+    data=[random.randint(0,100) for _ in range(100) ]
+    tsdata, trdata = divideData(data, crossValidation)
     
-ztsdata = delElems(data, ztrindex)
-print("sample ztsdata")
-print(ztsdata)
-print("training ztrdata")
-print(ztrdata)
+    print("initial data")
+    print(data)
+    print("sample tsdata")
+    print(tsdata)
+    print("training trdata")
+    print(trdata)
+
+main()
