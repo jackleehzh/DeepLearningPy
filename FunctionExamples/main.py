ce7c4ccd59c7a3fc6ddec9c059dab5eb52a07e75
liyinchen <liyc@liyinchen.com>
2019-01-04 14:44:26 +0800

    main函数的正常写法，异常的处理

---
+++ FunctionExamples/main.py
@@ -0,0 +1,21 @@
+import sys
+import getopt
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv = sys.argv):
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.GetoptError as msg:
+            raise Usage(msg)
+        #more code, unchanged
+    except Usage as err:
+        print(sys.stderr, err.msg)
+        print(sys.stderr, "for help use --help")
+        return 2
+
+if __name__ == "__main__":
+    sys.exit(main())
