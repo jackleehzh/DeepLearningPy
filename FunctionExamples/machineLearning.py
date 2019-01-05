efdddd370473766176517c93fe38bf97e2e52d49
liyinchen <liyc@liyinchen.com>
2019-01-05 10:06:35 +0800

    归类到文件夹

--- FunctionExamples/machineLearning.py
+++ FunctionExamples/machineLearning.py
@@ -1,9 +1,10 @@
 import numpy as np
 import Evaluation as elv
+import EvaluateStandard as es
 import random
 
-def generateData(maxNum, row, col):
-    return np.mat(np.random.randint(maxNum, size=(row, col)))
+def generateData(upperBound, row, col):
+    return np.mat(np.random.randint(upperBound, size=(row, col)))
 
 def divideLabel(data, col):
     index = col - 1
@@ -18,13 +19,6 @@ def linearRegression(trdx, trdy):
 def pridect(tsdx, w):
     return tsdx * w
 
-def evaluate(ret, tsdy):
-    errRate = 0
-    for i in range(len(ret)):
-        if abs(ret[i] - tsdy[i]) > 5:
-            errRate = errRate + 1
-    return 1 - float(errRate) / len(ret)
-
 def studyMachine(data, elvFunc, frequency):
     w = 0
     for i in range(frequency):
@@ -34,8 +28,8 @@ def studyMachine(data, elvFunc, frequency):
         #printData(data, trdata, tsdata)
         w = w + linearRegression(trdx, trdy)
     w = w / frequency
-    ret = pridect(tsdx, w)
-    return evaluate(ret, tsdy)
+    result = pridect(tsdx, w)
+    return es.precisionRecall(result, tsdy, 0.49, 1)
 
 def printData(data, trdata, tsdata):
     print("initial set")
@@ -49,7 +43,7 @@ def printResult(ret, tsdy):
     for i in range(len(ret)):
         print(ret[i], tsdy[i])
 
-data = generateData(100, 100, 6)
+data = generateData(2, 100, 6)
 
 result1 = studyMachine(data, elv.holdOut, 10)
 result2 = studyMachine(data, elv.crossValidation, 10)
@@ -57,4 +51,3 @@ result3 = studyMachine(data, elv.bootStrapping, 10)
 #printResult(ret, tsdy)
 
 print(result1, result2, result3)
-
