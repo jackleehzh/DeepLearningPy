32559765a7723291c7d030836e518092707c12ac
liyinchen <liyc@liyinchen.com>
2019-03-16 09:07:35 +0800

    调整打印输出；调整函数组合；改小提醒时间间隔方便测试

--- pku/pku.py
+++ pku/pku.py
@@ -40,12 +40,12 @@ def remindMe(list0, dict0):
         dict1 = {}
         for key in dict2:
             t2 = t - dict2[key]
-            if t2 >= 25 and t2 <= 30:
+            if t2 >= 2 and t2 <= 4:
                 if maxnum < t2:
                     maxnum = t2
                 dict1[key] = t2
                 
-        t1 = 30 - maxnum
+        t1 = 4 - maxnum
         if t1 > 0:
             for key in dict1:
                 dict1[key] = dict1[key] + t1
@@ -54,14 +54,31 @@ def remindMe(list0, dict0):
             remind.remind(dict1, list0, dict0)
             time.sleep(2)
 
+def printCMD():
+    print('b.\t返回上一层')
+    print('a.\t增加内容')
+    print('d.\t删除内容')
+    print('m.\t修改内容')
+    print('e.\t退出')
+    print('-------------------')
+
+def remindThread(list0, dict0):
+    t = threading.Thread(target=remindMe,args=(list0, dict0))
+    t.setDaemon(True) ## thread1,它做为程序主线程的守护线程,当主线程退出时,thread1线程也会退出,由thread1启动的其它子线程会同时退出,不管是否执行完任务
+    t.start()
+
+def update(list3, list2):
+    list2 = rw.updateInfo(list3, list2)
+    dict2 = getTime(list2)
+    rw.writeFile(filename2, list2)
+    return dict2
+
 def show(filename1, filename2):
     list0, dict0 = rw.loadData(filename1)
     list2 = rw.loadInfo(filename2)
     global dict2
     dict2 = getTime(list2)
-    t = threading.Thread(target=remindMe,args=(list0, dict0))
-    t.setDaemon(True) ## thread1,它做为程序主线程的守护线程,当主线程退出时,thread1线程也会退出,由thread1启动的其它子线程会同时退出,不管是否执行完任务
-    t.start()
+    remindThread(list0, dict0)
     layer = 0
     uplayerBeginline = 0
     sameLayer = False
@@ -74,7 +91,6 @@ def show(filename1, filename2):
         for a in list0:
             c = a.count('	')
             a = a.strip()
-            
             if begin and c < layer:
                 uplayerBeginline = list3[0]
                 break
@@ -86,23 +102,13 @@ def show(filename1, filename2):
             if c == layer and count >= uplayerBeginline:
                 begin = True
                 if count2 == 0:
-                    print('b.\t返回上一层')
-                    print('a.\t增加内容')
-                    print('d.\t删除内容')
-                    print('m.\t修改内容')
-                    print('e.\t退出')
-                    print('-------------------')
+                    printCMD()
                 count2 = count2 + 1                      
                 print(str(count2) + '.\t' + a[:a.find('【')])
                 list3.append(count)
- 
             count = count + 1
                 
-        
-        #print(list3)
-        list2 = rw.updateInfo(list3, list2)
-        dict2 = getTime(list2)
-        rw.writeFile(filename2, list2)
+        dict2 = update(list3, list2)
         num = input("请输入：")
         if num == 'e':
             return
